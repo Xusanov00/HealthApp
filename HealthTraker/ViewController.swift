072b2dfbc33b78cdcb7c92b8add591950c47762d
Mukhammadali Khusanov <mukhammadali.khu@payme.uz>
2025-12-10 23:52:36 +0500

    Initial Commit

---
+++ HealthTraker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthTraker
+//
+//  Created by Ali on 10/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
